7893439cfe635b462cf206097a92f117827bdc5b
jc-hiroto <taste.ful.msa@gmail.com>
2020-06-19 20:00:59 +0800

    Add home method

--- courseApp/src/home.java
+++ courseApp/src/home.java
@@ -1,2 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class home {
+    private JPanel window;
+    public static String metalUI = "javax.swing.plaf.metal.MetalLookAndFeel";
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(home.metalUI); // 使用Metal UI 模式啟動
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame("Course"); // 設定視窗標題
+        windowSizeLimiter(frame, 800,800);
+        frame.setContentPane(new home().window);
+        frame.pack();
+        frame.getContentPane().requestFocusInWindow();
+        frame.setSize(1000,800); // 設定初始視窗大小
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    private static void windowSizeLimiter(JFrame frame, int i, int i1) {
+    }
 }
