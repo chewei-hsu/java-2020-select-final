3c0758fcc93134ad49aac6baf09be3f4329e6688
Tetrapod1206 <61351385+Tetrapod1206@users.noreply.github.com>
2020-06-19 21:51:00 +0800

    BROKEN

--- courseApp/src/test.java
+++ courseApp/src/test.java
@@ -5,6 +5,7 @@ public class test {
     private JPanel test1;
 
     public static void main(String[] args) {
+        System.out.println(Processor.timeWordToInt("一4,5,6 二2,A"));
         Debugger.setDebugMode(true);
         try {
             UIManager.setLookAndFeel(home.metalUI); // 使用Metal UI 模式啟動
