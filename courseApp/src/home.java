4e9057b77101fbda85b2c5fc925d41884e3ff2a0
jc-hiroto <38657003+jc-hiroto@users.noreply.github.com>
2020-06-21 23:34:37 +0800

    Finish comment of home.java

--- courseApp/src/home.java
+++ courseApp/src/home.java
@@ -9,10 +9,8 @@ import java.util.ArrayList;
 
 public class home {
     private static JFrame frame = new JFrame("Course"); // 設定視窗標題
-
     private JPanel searchBarAndBtn;
     private JPanel searchResult;
-    //private JPanel resultHolder = new JPanel();
     private JPanel selected;
     private JPanel panel1;
     private JTextField searchField;
@@ -51,9 +49,10 @@ public class home {
     }
     public int currentSemesterIndex = semesterSelect.getSelectedIndex();
     public home() {
-        initDetailElement();
-        selected.setVisible(false);
+        initDetailElement();  // initialize elements in the detail panel.
+        selected.setVisible(false); // hide detail panel when startup.
         searchResult = resultList.createPanel(DB.getCourse(null, semesterSelect.getSelectedItem().toString(),isMustSelect.getSelectedIndex()));
+        // initialize all cardLayouts.
         tableDisplay = new courseTable().getPanel();
         selected.setBorder(new LineBorder(Color.GRAY, 3));
         resultHolder.add(searchResult,"r");
@@ -63,6 +62,7 @@ public class home {
         tableLayout = (CardLayout)tableHolder.getLayout();
         layout.show(resultHolder,"r");
         tableLayout.show(tableHolder,"t");
+        // btn listeners.
         btnSearch.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
@@ -82,10 +82,12 @@ public class home {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 if(choosedCourse.size() == 0){
+                    // check if the choosedcourse array is empty.
                     JOptionPane.showMessageDialog(null,"請先選擇至少一門課程再使用此功能","你沒有必帶嗎",JOptionPane.ERROR_MESSAGE);
                 }
                 else{
                     try{
+                        // use auto choose course to automatically fill in courses.
                         Processor.autoChooseCourse(semesterSelect.getSelectedItem().toString());
                         refreshTable(courseStats);
                     }catch(NullPointerException ne){
@@ -94,6 +96,7 @@ public class home {
                 }
             }
         });
+        // search field hint text handler.
         searchField.addFocusListener(new FocusAdapter() {
             @Override
             public void focusGained(FocusEvent e) {
@@ -102,7 +105,7 @@ public class home {
                 searchField.setForeground(new Color(8, 37, 42));
             }
         });
-
+        // search field hint text handler.
         searchField.addFocusListener(new FocusAdapter() {
             @Override
             public void focusLost(FocusEvent e) {
@@ -116,13 +119,14 @@ public class home {
                 }
             }
         });
+        // keyboard enter listener for search trigger.
         searchField.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 super.keyPressed(e);
                 int keyCode = e.getKeyCode();
-                if(keyCode == 10){
-                    //Enter button
+                if(keyCode == 10){  //Enter button
+                    // handle debug mode switch.
                     if(searchField.getText().equals("debug -on")){
                         if(Debugger.getDebugMode()){
                             JOptionPane.showMessageDialog(null,"開發者模式已經是開啟狀態\n如需關閉請輸入 debug -off","開發者模式",JOptionPane.ERROR_MESSAGE);
@@ -147,12 +151,14 @@ public class home {
 
                     }
                     else{
+                        // if field text is not debug command, then search.
                         search();
                     }
                 }
             }
         });
 
+        // check if semester is changed, if changed then show a warning and clear result and table.
         semesterSelect.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -173,6 +179,7 @@ public class home {
         });
     }
 
+    // method of the search function.
     public void search(){
         if(searchField.getText().equals("輸入關鍵字 或 直接搜尋")){
             searchCourse = null;
@@ -195,6 +202,7 @@ public class home {
         }
     }
 
+    // main function, the start of the GUI application.
     public static void main(String[] args) {
         Debugger.setDebugMode(false);
         try {
@@ -211,6 +219,9 @@ public class home {
         frame.setVisible(true);
         windowSizeLimiter(frame, 1000, 900);
     }
+
+    // method of refresh course table by new data.
+    // use cardLayout to resolve the refresh problem.
     public void refreshTable(ArrayList<ArrayList<CourseData>> data){
         tableHolder.remove(tableDisplay);
         tableDisplay = null;
@@ -219,6 +230,8 @@ public class home {
         tableLayout = (CardLayout)tableHolder.getLayout();
         tableLayout.show(tableHolder,"t");
     }
+    // method of refresh search result panel by new data.
+    // use cardLayout to resolve the refresh problem.
     public void refreshDetailPanel(){
         if(detailData != null){
             selected.setVisible(true);
@@ -255,6 +268,8 @@ public class home {
 
         }
     }
+
+    // make sure all the text in detail panel is empty during startup.
    public void initDetailElement(){
         lbName.setText("");
         lbLocation.setText("");
@@ -266,7 +281,7 @@ public class home {
         textPs.setText("");
    }
 
-
+    // limit the window size in order to make the app runs correctly.
     private static void windowSizeLimiter(JFrame frame, int width, int height) {
         frame.addComponentListener(new ComponentAdapter() {
             public void componentResized(ComponentEvent evt) {
@@ -279,16 +294,19 @@ public class home {
         });
     }
 
+    // intellij custom create element function.
     private void createUIComponents() {
     }
 
+    // inner class renderer.
+    // method used to render the custom defined JList (using custom cell elements).
     class JListCustomRenderer extends JFrame {
         private ArrayList<CourseData> displayData = new ArrayList<CourseData>();
 
         public JListCustomRenderer() {
-
         }
 
+        // create the panel then return.
         public JPanel createPanel(ArrayList<CourseData> CD) {
             if (CD != null) {
                 Debugger.showDebugMessage(CD.size() + "");
@@ -300,14 +318,16 @@ public class home {
             JPanel panel = new JPanel(new BorderLayout());
             JList jlist = createList();
 
+            // the mouse click listener.
             MouseListener mouseListener = new MouseAdapter() {
                 public void mouseClicked(MouseEvent mouseEvent) {
                     JList<String> theList = (JList) mouseEvent.getSource();
-                    if (mouseEvent.getClickCount() == 1) {
+                    if (mouseEvent.getClickCount() == 1) { // Single clicked.
                         int index = theList.locationToIndex(mouseEvent.getPoint());
                         if (index >= 0) {
                             Object o = theList.getModel().getElementAt(index);
                             Debugger.showDebugMessage("Single-clicked on: " + o);
+                            // refresh detail panel if single clicked on the element.
                             for (int i = 0; i < displayData.size(); i++) {
                                 if (displayData.get(i).getRandom_num().equals(o.toString())) {
                                     detailData = displayData.get(i);
@@ -317,12 +337,12 @@ public class home {
                             }
                         }
                     }
-                    if (mouseEvent.getClickCount() == 2) {
+                    if (mouseEvent.getClickCount() == 2) { // Double clicked.
                         int index = theList.locationToIndex(mouseEvent.getPoint());
                         if (index >= 0) {
                             Object o = theList.getModel().getElementAt(index);
                             Debugger.showDebugMessage("Double-clicked on: " + o);
-
+                            // add or remove  the course to array when double clicked on the element.
                             for (int i = 0; i < displayData.size(); i++) {
                                 if (displayData.get(i).getRandom_num().equals(o.toString())) {
                                     detailData = displayData.get(i);
@@ -339,10 +359,12 @@ public class home {
                                 }
                             }
                             Debugger.showDebugMessage("Choosed course: "+choosedCourse);
+                            // determine if the course needs to be added or removed.
                             if(isExist){
                                 choosedCourse.remove(target);
                             }
                             else{
+                                // check if there's too much (>2) course at the same time block.
                                 if(Processor.courseAddCheck(courseStats,detailData.getTime())){
                                     choosedCourse.add(detailData);
                                 }
@@ -366,6 +388,7 @@ public class home {
             return frame;
         }
 
+        // create the JList.
         public JList<CourseData> createList() {
             DefaultListModel<CourseData> model = new DefaultListModel<CourseData>();
             for (CourseData val : displayData) {
